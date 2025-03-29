864270ceb9814be5a6b5b80962b59baf084aac53
philippart-s <wildagsx@gmail.com>
2025-03-29 10:29:36 +0000

    fix: 🐛 whitelist DB IPs

--- 0.01-source-env.sh
+++ 0.01-source-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ls
+
 # Source environment variables
 set -a
-source .talk.env
-source .env
+source $SCRIPT_DIR/.talk.env
+source $SCRIPT_DIR/.env
 set +a
--- jarvis-iac/2.17-iac-pulumi-up.sh
+++ jarvis-iac/2.17-iac-pulumi-up.sh
@@ -8,16 +8,25 @@ clear
 bat -P -r 10: $(basename "$0")
 
 # 🏗️  Create the infrastructure 🏗️
-pulumi up
+#pulumi up
 
 # 🛠️  Set env variables 🛠️
-sudo sed -i "s/^OVH_DB_HOST=.*/OVH_DB_HOST=$(pulumi stack output db_host --non-interactive)/" ../.env
-sudo sed -i "s/^OVH_DB_PASSWORD=.*/OVH_DB_PASSWORD=$(pulumi stack output avnadmin-password --show-secrets --non-interactive)/" ../.env
+sudo sed -i "s/^OVH_DB_HOST=.*/OVH_DB_HOST=$(pulumi stack output db_host --non-interactive)/" ../.talk.env
+sudo sed -i "s/^OVH_DB_PASSWORD=.*/OVH_DB_PASSWORD=$(pulumi stack output avnadmin-password --show-secrets --non-interactive)/" ../.talk.env
 
 ## 🛠️  Generate kubeconfig-ovh file 🛠️
 pulumi stack output kubeconfig --show-secrets --non-interactive > ../jarvis-operator/kubeconfig-ovh.yml
 
 ## ☸️ Set Kubernetes Node external IP ☸️
 export KUBECONFIG=../jarvis-operator/kubeconfig-ovh.yml
-NODE_IP=kubectl get nodes -o jsonpath='{.items[].status.addresses[].address}'
-sudo sed -i "s/^NODE_PUBLIC_IP=.*/NODE_PUBLIC_IP=$($NODE_IP)/" ../.env
+NODE_IP=$(kubectl get nodes -o jsonpath='{.items[].status.addresses[].address}')
+sudo sed -i "s/^NODE_PUBLIC_IP=.*/NODE_PUBLIC_IP=$NODE_IP/" ../.talk.env
+
+## 💿 DB whitelist 💿
+LOCAL_IP=$(curl ifconfig.me)
+curl -X PUT "https://eu.api.ovh.com/v1/cloud/project/$OVH_CLOUD_PROJECT_SERVICE/database/postgresql/$OVH_DB_ID" \
+ -H "accept: application/json"\
+ -H "authorization: Bearer $OVHCLOUD_TOKEN"\
+ -H "content-type: application/json" \
+ -d "{\"ipRestrictions\":[{\"description\":\"kube_node_ip\",\"ip\":\"$NODE_IP/32\"}, {\"description\":\"local_ip\",\"ip\":\"$LOCAL_IP/32\"}]}" > /dev/null
+
