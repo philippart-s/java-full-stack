ad511f021df8786d207d5cb2aaf0168da2f8b9a7
philippart-s <wildagsx@gmail.com>
2025-04-02 22:10:56 +0200

    test: ⚗️ shared libraries

--- jarvis-ci/vars/jarvis.groovy
+++ jarvis-ci/vars/jarvis.groovy
@@ -8,3 +8,10 @@ def ghTag(String userName, String password, long buildNumber) {
     sh "git push origin v${buildNumber}"
 }
 
+def ghRelease(String userName, String token, String buildNumber) {
+    sh "curl -X POST \
+         https://api.github.com/repos/${userName}/java-full-stack/releases \
+         -H \"Authorization: Bearer ${token}\" \
+         -H \"Content-Type: application/json\" \
+         -d \"{\"tag_name\": \"v${buildNumber}\", \"name\": \"Release ${buildNumber}\", \"draft\": false, \"prerelease\": false}\""
+}
