d80bae608d1e1095781b7eeae8293376f32af44b
philippart-s <wildagsx@gmail.com>
2025-04-02 21:56:36 +0200

    test: ⚗️ shared libraries

--- jarvis-ci/vars/jarvis.groovy
+++ jarvis-ci/vars/jarvis.groovy
@@ -1,7 +1,3 @@
-def buildMaven() {
-    sh 'mvn clean package'
-}
-
 def tag(String userName, String password, long buildNumber) {
     sh "git remote set-url origin https://${userName}:${password}@github.com/${userName}/java-full-stack.git"
     sh "git config --global user.password \"${password}\""
@@ -10,4 +6,12 @@ def tag(String userName, String password, long buildNumber) {
     sh 'git config -l'
     sh "git tag -a v\${buildNumber} -m \"🏷️ Release ${buildNumber}\""
     sh "git push origin v${buildNumber}"
+}
+
+def release(String userName, String password, long buildNumber) {
+    sh "curl -X POST \
+         https://api.github.com/repos/${userName}/java-full-stack/releases \
+         -H \"Authorization: Bearer ${password}\" \
+         -H \"Content-Type: application/json\" \
+         -d \"{\"tag_name\": \"v${buildNumber}\", \"name\": \"Release ${buildNumber}\", \"draft\": false, \"prerelease\": false}\""
 }
