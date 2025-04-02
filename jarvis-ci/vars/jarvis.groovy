7b3979f1d15f5992c14a450010432c3c6ece9217
philippart-s <wildagsx@gmail.com>
2025-04-02 21:44:31 +0200

    test: ⚗️ shared libraries

--- jarvis-ci/vars/jarvis.groovy
+++ jarvis-ci/vars/jarvis.groovy
@@ -1,3 +1,13 @@
 def buildMaven() {
     sh 'mvn clean package'
 }
+
+def tag(String userName, String password, long buildNumber) {
+    sh "git remote set-url origin https://${userName}:${password}@github.com/${userName}/java-full-stack.git"
+    sh "git config --global user.password \"${password}\""
+    sh "git config --global user.name \"${userName}\""
+    sh 'git config --global user.email "wildagsx@gmail.com"'
+    sh 'git config -l'
+    sh "git tag -a v\${buildNumber} -m \"🏷️ Release ${buildNumber}\""
+    sh "git push origin v${buildNumber}"
+}
