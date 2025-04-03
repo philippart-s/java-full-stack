d50e5b30e9c3eff7a4098bf9c3999430f5e59123
philippart-s <wildagsx@gmail.com>
2025-04-03 08:56:43 +0200

    test: ⚗️ shared libraries

--- jarvis-ci/vars/jarvis.groovy
+++ jarvis-ci/vars/jarvis.groovy
@@ -1,17 +1,21 @@
 def ghTag(String userName, String password, long buildNumber) {
-    sh "git remote set-url origin https://${userName}:${password}@github.com/${userName}/java-full-stack.git"
+    echo 'ghTag'
+    /*sh "git remote set-url origin https://${userName}:${password}@github.com/${userName}/java-full-stack.git"
     sh "git config --global user.password \"${password}\""
     sh "git config --global user.name \"${userName}\""
     sh 'git config --global user.email "wildagsx@gmail.com"'
     sh 'git config -l'
     sh "git tag -a v\${buildNumber} -m \"🏷️ Release ${buildNumber}\""
-    sh "git push origin v${buildNumber}"
+    sh "git push origin v${buildNumber}"*/
 }
 
 def ghRelease(String userName, String token, String buildNumber) {
-    sh "curl -X POST \
+    echo 'ghRelease'
+    /*sh "curl -X POST \
          https://api.github.com/repos/${userName}/java-full-stack/releases \
          -H \"Authorization: Bearer ${token}\" \
          -H \"Content-Type: application/json\" \
          -d \"{\"tag_name\": \"v${buildNumber}\", \"name\": \"Release ${buildNumber}\", \"draft\": false, \"prerelease\": false}\""
+
+     */
 }
